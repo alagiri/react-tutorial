444a3ce8b2eced39faab4a2d836edf4c223034ba
Alagiri <alagiri@gmail.com>
2020-07-12 19:30:17 -0700

    Updates

---
+++ precache-manifest.f9202e47ed3830a96234dfed4f6b5d5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bec4b13b48e993ef79ce155be2e87d",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "75d6ec6d7234b4530a87",
+    "url": "/react-tutorial/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "f3a8912e9639c9ea3ccc",
+    "url": "/react-tutorial/static/js/2.1bbe048d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.1bbe048d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d6ec6d7234b4530a87",
+    "url": "/react-tutorial/static/js/main.bf8ebda0.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
